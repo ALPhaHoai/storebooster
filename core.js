d6e8e77f6520fd16417e83b9f5e5ffcf22800abb
Drake <drake@eup.net.vn>
2024-11-26 15:19:02 +0700

    commit

--- core.js
+++ core.js
@@ -1,46 +1,54 @@
-import { requireDB } from "./db.js";
 import SteamClient from "steamutils/SteamClient.js";
+import axios from "axios";
+import { logNow } from "steamutils/utils.js";
 
+let steamClient = null;
 export async function initStoreBooster() {
   console.log("initStoreBooster");
 
-  const accounts = await new Promise((resolve) => {
-    requireDB(async function (collection) {
-      resolve(
-        await collection.MyAccount.aggregate([
-          {
-            $match: {
-              "config.store": true,
-              prime: {
-                $ne: true,
-              },
-            },
-          },
-          {
-            $sample: {
-              size: 100,
-            },
-          },
-          {
-            $project: {
-              cookie: 1,
-            },
-          },
-        ]).toArray(),
-      );
-    });
-  });
+  boostAccounts();
+  setInterval(boostAccounts, 10 * 60000);
+}
 
-  for await (const account of accounts) {
-    const client = new SteamClient({
-      cookie: account.cookie,
-    });
-    const playable = await client.playCSGO();
-    client.offAllEvent();
-    if (playable) {
+async function boostAccounts() {
+  const accounts = await getRandomStoreMyAccount();
+  if (!Array.isArray(accounts)) {
+    return;
+  }
+
+  while (accounts.length) {
+    const client = await boostAccount(accounts.pop());
+    if (client) {
+      steamClient?.logOff();
+      steamClient = client;
       break;
-    } else {
-      await client.logOff();
     }
   }
+}
+
+async function boostAccount(account) {
+  if (!account?.cookie) {
+    return;
+  }
+  logNow("boosting account", boostAccount._id);
+  const client = new SteamClient({
+    cookie: account.cookie,
+  });
+  const playable = await client.playCSGO();
+  client.offAllEvent();
+  if (playable) {
+    return client;
+  } else {
+    await client.logOff();
+  }
+}
+
+async function getRandomStoreMyAccount() {
+  try {
+    return (
+      await axios.get(
+        `${process.env.API_ENDPOINT}/getRandomStoreMyAccount?limit=20`,
+      )
+    )?.data?.result;
+  } catch (e) {}
 }
